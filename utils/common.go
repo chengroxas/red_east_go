93f8870ffcd212c488af6c06a744ec7d0e4743ac
weichengzhong <weichengzhong@hk01.com>
2020-04-15 18:38:56 +0800

    将错误码分开

--- utils/common.go
+++ utils/common.go
@@ -15,25 +15,3 @@ var (
 	Config config.Config
 	DB     *gorm.DB
 )
-
-var (
-	CODE_BAD_PARAM       = 400
-	CODE_REQUIRE_AUTH    = 401
-	CODE_PERSIMMON_ERROR = 403
-	CODE_NOT_EXIST       = 404
-	CODE_METHOD_NOT_ALL  = 405
-	CODE_WRONG_ITEM      = 406
-)
-
-var errorCodeMsg = map[int]string{
-	CODE_BAD_PARAM:    "请校验参数",
-	CODE_REQUIRE_AUTH: "需要验证",
-}
-
-func GetErrorCodeMsg(code int) string {
-	if value, ok := errorCodeMsg[code]; ok {
-		return value
-	} else {
-		return ""
-	}
-}
